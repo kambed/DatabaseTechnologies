ac0343f6b309a4ff6463a787d90bbe5e0bb93b74
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2024-03-18 17:33:25 +0100

    Delete Spring config

--- task3/src/main/java/com/kambed/docker/logic/exception/MessageNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.kambed.docker.logic.exception;
-
-public class MessageNotFoundException extends RuntimeException {
-    public MessageNotFoundException(String message) {
-        super(message);
-    }
-}
