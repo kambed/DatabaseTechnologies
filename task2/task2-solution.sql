b246f20869cf281da15720b1301dc576d2823cc3
Damian Biskupski <ddamian2057@gmail.com>
2024-03-19 13:00:03 +0100

    Update task2-solution.sql

--- task2/task2-solution.sql
+++ task2/task2-solution.sql
@@ -72,9 +72,44 @@ EXEC LoadSpatialData 'BusStop1', 'POINT(1 1)', 'point';
 
 ------------------------------------- 4 -------------------------------------
 
+-- build geometric area
 
+SELECT DISTINCT
+    bs.id AS BusStopID,
+    bs.name AS BusStopName,
+    bl.id AS BusLoopID,
+    bl.name AS BusLoopName,
+    bw.id AS BusWayID,
+    bw.name AS BusWayName
+FROM
+    BusStop bs
+LEFT JOIN
+    BusLoop bl
+ON
+    bs.geom.STIntersects(bl.geom) = 1
+LEFT JOIN
+    BusWay bw
+ON
+    bs.geom.STIntersects(bw.geom) = 1;
 
+-- connect geometric area with coordinate system
 
+CREATE OR ALTER PROCEDURE ProjectGeometryToDifferentSRID
+    @geometry GEOMETRY,
+    @targetSRID INT
+AS
+BEGIN
+    DECLARE @projectedGeometry GEOMETRY;
+    SET @projectedGeometry = @geometry.STAsText();
+    SET @projectedGeometry = geometry::STGeomFromText(@projectedGeometry.ToString(), @targetSRID);
 
+    SELECT @projectedGeometry AS ProjectedGeometry;
+END;
 
+DECLARE @inputGeometry GEOMETRY;
+DECLARE @targetSRID INT;
+SET @inputGeometry = (SELECT TOP 1 geom FROM BusLoop);
+SET @targetSRID = 4326;
+
+EXEC ProjectGeometryToDifferentSRID @geometry = @inputGeometry, @targetSRID = @targetSRID;
 ------------------------------------- 5 -------------------------------------
